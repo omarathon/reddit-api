3b8a7977978195df5b77f28655832fcd6e9d1618
omarathon <pixzlert@gmail.com>
2020-01-01 09:38:06 +0000

    v1.1 changes:
    - fixed NullPointerException generated from JRAW for Linux environments where the default ProxySelector is null for OkHttp
    
    - (optimised imports)

--- src/main/java/dev/omarathon/redditapi/helper/Reddit.java
+++ src/main/java/dev/omarathon/redditapi/helper/Reddit.java
@@ -9,6 +9,15 @@ import net.dean.jraw.http.OkHttpNetworkAdapter;
 import net.dean.jraw.http.UserAgent;
 import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
+import okhttp3.OkHttpClient;
+
+import java.io.IOException;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 
 public class Reddit {
     public static RedditClient connect(AuthData auth) {
@@ -29,8 +38,23 @@ public class Reddit {
                 credentialsData.getClientSecret()
         );
 
-        NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
+        ProxySelector proxySelector = ProxySelector.getDefault() == null ? new NullProxySelector() : ProxySelector.getDefault();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().proxySelector(proxySelector).build();
+
+        NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent, okHttpClient);
 
         return OAuthHelper.automatic(adapter, credentials);
+    }
+
+    private static class NullProxySelector extends ProxySelector {
+        @Override
+        public List<Proxy> select(URI uri) {
+            return Collections.singletonList(Proxy.NO_PROXY);
+        }
+
+        @Override
+        public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {
+            // pass
+        }
     }
 }
